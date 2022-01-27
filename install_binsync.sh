a5e651b3c20b42a7a7fe21b87c6b53785c5aecb3
mooleshacat <43627985+leshacat@users.noreply.github.com>
2022-01-26 19:36:01 -0500

    Added code to clear ENV vars for security reasons

--- install_binsync.sh
+++ install_binsync.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
+echo ""
+echo "binsync-push ${vDIRECTORY}"
+echo ""
 
+PWD=$(pwd)
+if test -f "${PWD}/config.inc"; then
+  source ${PWD}/config.inc
+else
+  echo "${PWD}/config.inc does not exist, please read the README.md!"
+  echo ""
+  exit
+fi
+
+# Install binsync
+
+# Clear ENV vars for security
+source ${PWD}/config.clear.inc
