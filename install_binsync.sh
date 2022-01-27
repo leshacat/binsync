14bddb5f421ffb8cf5fa469590a2b12cce71fc98
mooleshacat <43627985+leshacat@users.noreply.github.com>
2022-01-26 19:57:28 -0500

    initial installer coding

--- install_binsync.sh
+++ install_binsync.sh
@@ -13,7 +13,25 @@ else
   exit
 fi
 
-# Install binsync
+# Get / fill variables
+vPWD=$(pwd)
+vPATH=$(echo $PATH)
+
+# Make the directory where bins will reside
+mkdir -p ${vDIRECTORY}
+
+# Copy the binsync scripts
+cp ${vPWD}/binsync-* ${vDIRECTORY}
+
+# Check if vDIRECTORY is added to $PATH
+if [[ "${vDIRECTORY}" != *"${vPATH}"* ]]; then
+  echo ""
+  echo "Adding binsync directory to \$PATH"
+  echo "export PATH=${HOME}/bin:\$PATH" | tee -a ${HOME}/.bashrc
+  echo ""
+  echo "Please execute 'source ${HOME}/.bashrc' or log out and back in again"
+  echo ""
+fi
 
 # Clear ENV vars for security
 source ${PWD}/config.clear.inc
